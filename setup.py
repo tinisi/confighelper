784305de791add7270d546fbadbdbce210500696
Jason Grosz <jgrosz@tinisi.com>
2013-12-05 22:18:35 -0600

    got package working OK (as far as I can tell)

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
 
 setup(
+    author='Jason Grosz',
+    author_email='jgrosz@tinisi.com',
     name='confighelper',
-    version='0.1dev',
-    packages=['confighelper',],
+    version='0.16dev',
+    packages = ['confighelper'],
     license='BSD',
     long_description=open('README.md').read(),
+    url='http://github.com/tinisi/confighelper'
 )
